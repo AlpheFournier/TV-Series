cb80bfec5af4ec29e5ec840f3409461dcc978a35
AlpheFournier <alphe.fournier@gmail.com>
2017-10-10 17:04:50 +0200

    Fin journée 10/10

--- Actor.py
+++ Actor.py
@@ -1,14 +1,20 @@
 class Person:
 
     list_of_person = {}
+    ident = 1000000
 
-    def __init__(self,name,age, nationality, biographie, tv_series_actor, tv_series_productor, tv_series_director):
+    def __init__(self,id,name,age, nationality, biographie, tv_series_actor, tv_series_productor, tv_series_director):
+        if Person.ident > 9999999:
+            raise Exception("Too many People")
 
+        self.id = Person.ident
+        Person.ident += 1
         self.name = name
         self.age = int(age)
         self.nationality = nationality
         self.biographie = biographie
-        self.tv_series_actor = tv_series_actor[0]
-        self.tv_series_productor = tv_series_productor[0]
-        self.tv_series_director = tv_series_director[0]
-        
+        self.tv_series_actor = []
+        self.tv_series_productor = []
+        self.tv_series_director = []
+        # on met 3 tableaux pour séparer les films dans lequel la star a officié en tant qu'acteur, producteur ou
+        # réalisateur (souvent, les personnes auront 2 tableaux de vide, mais pour Woody Allen par exemple...)
--- TV_Chain.py
+++ TV_Chain.py
@@ -1,4 +1,13 @@
 class TV_Chain:
 
-    def __init__(self, name):
+    ident = 1000000
+
+    def __init__(self, id, name, list_of_series):
+        if TV_Chain.ident > 9999999:
+            raise Exception ("Too many chains")
+
+        self.id = TV_Chain.ident
+        TV_Chain.ident += 1
         self.name = name
+        self.list_of_series = []
+
