ef7680b14a2572b35b442ca670162a8393b4d41d
AlpheFournier <alphe.fournier@gmail.com>
2017-10-10 15:52:39 +0200

    Modif Auteurs + TV_Chain

--- Actor.py
+++ Actor.py
@@ -1,6 +1,14 @@
-class Actor:
+class Person:
 
-    def __init__(self,name,age):
+    list_of_person = {}
+
+    def __init__(self,name,age, nationality, biographie, tv_series_actor, tv_series_productor, tv_series_director):
 
         self.name = name
-        self.age = age
+        self.age = int(age)
+        self.nationality = nationality
+        self.biographie = biographie
+        self.tv_series_actor = tv_series_actor[0]
+        self.tv_series_productor = tv_series_productor[0]
+        self.tv_series_director = tv_series_director[0]
+        
---
+++ TV_Chain.py
@@ -0,0 +1,4 @@
+class TV_Chain:
+
+    def __init__(self, name):
+        self.name = name
