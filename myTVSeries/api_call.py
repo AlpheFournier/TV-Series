a6a75f2646cd2d2c3bec888fec7b7054e50a81a4
AlpheFournier <32547095+alphefournier@users.noreply.github.com>
2017-11-06 16:42:20 +0100

    Prepare merge

--- myTVSeries/api_call.py
+++ myTVSeries/api_call.py
@@ -35,20 +35,12 @@ class Api_call:
             answer.append(element['name'])
         return answer
 
-    def want_picture(self):
-        # We use id=1399, it corresponds to Game of thrones, and we can't use a random because there is some series which doesn't have any associated picture...
-        resp = requests.get(Api_call.url + "tv/1399/images" + Api_call.api_key)
+    def want_picture(self, query):
+        resp = requests.get(Api_call.url + "tv/" + str(query) + "/images" + Api_call.api_key)
         ans = resp.json()
         image = ans.get('backdrops')[0].get('file_path')
         url_image = "https://image.tmdb.org/t/p/w640" + image
         return(url_image)
-
-    def element_serie(self, id):
-        resp = requests.get(Api_call.url + "tv/" + str(id) + Api_call.api_key)
-        answer = []
-        for element in resp.json()['answer']:
-            answer.append(element)
-        return answer
 
     def discover_serie(self):
         resp = requests.get (Api_call.url + "discover/tv/" + Api_call.api_key + "&sor_by=vote_average.desc&page=1&include_null_first_air_dates=false'")
