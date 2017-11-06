0911993d834c27d0e505a92c8867d788143c7117
maximallain <32595427+maximallain@users.noreply.github.com>
2017-11-06 17:10:32 +0100

    Update settings.py

--- myTVSeries/myTVSeries/settings.py
+++ myTVSeries/myTVSeries/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'LikeSeries.apps.LikeseriesConfig',
+    'signUp.apps.SignupConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'myTVSeries.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # (Max) à modifier, peut être par :
+        #        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'myTVSeries/../templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# (Max) A modifier éventuellement
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'home'
