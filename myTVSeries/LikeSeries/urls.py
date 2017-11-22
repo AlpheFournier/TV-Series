5d30dff530b56978ee4f9af2f6298ce2c8239df3
maximallain <32595427+maximallain@users.noreply.github.com>
2017-11-22 12:08:10 +0100

    Matou a rajouté Remove

--- myTVSeries/LikeSeries/urls.py
+++ myTVSeries/LikeSeries/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', search, name = 'index'),
     url(r'^TVShow_page/(?P<tv_id>\d+)/$', TVShowPage, name='TVShow_page'),
     url(r'^TVShow_page/Like/(?P<tv_id>\d+)/$', Save_like, name ='Like'),
+    url(r'^TVShow_page/Remove/(?P<tv_id>\d+)/$', Remove_like, name='Remove'),
     url(r'^TVShow_page/Bibliotheque/$', Afficher_series_liked, name ='Bibliotheque'),
     url(r'^TVShow_page/(?P<tv_id>\d+)/(?P<season_number>\d+)/$', Season_Page, name='Season_Page'),
     url(r'^TVShow_page/(?P<tv_id>\d+)/(?P<season_number>\d+)/(?P<episode_number>\d+)/$', Episode_Page, name='Episode_Page'),
